d5a1393271af7ce38b971128e595d846128043ca
Will Brown <wtbrown10@gmail.com>
2020-04-07 21:28:41 -0400

    added a thired array and made consolelog longer

--- fruitsales.js
+++ fruitsales.js
@@ -1,6 +1,9 @@
 let fruitInventory = ["Apples", "Peaches", "Pumpkins", "Endives"];
 let fruitPrices = [1, 1.5, 7, 12];
+let fruitSold = [35, 22, 7, 1];
 
 for (i = 0; i < fruitInventory.length; i++) {
-  console.log(`${fruitInventory[i]}`);
+  console.log(
+    `We have ${fruitInventory[i]} for sale at $${fruitPrices[i]} each. and we have already sold ${fruitSold[i]} today!`
+  );
 }
